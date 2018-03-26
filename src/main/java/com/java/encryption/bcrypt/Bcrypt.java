61096fb86aaaf2db7b8fabc1416bfe6f47c3c591
HGChamara <hgchamara1991@gmail.com>
2018-03-26 14:44:16 +0530

    BCrypt haching

--- src/main/java/com/java/encryption/bcrypt/Bcrypt.java
+++ src/main/java/com/java/encryption/bcrypt/Bcrypt.java
@@ -1,10 +1,18 @@
 package com.java.encryption.bcrypt;
 
+import org.mindrot.jbcrypt.BCrypt;
+
 public class Bcrypt {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void main(String[] args) 
+	{
+		String plainText = "password_test";
+		String generatedHash = BCrypt.hashpw(plainText, BCrypt.gensalt(5));
+		boolean isMatched = BCrypt.checkpw(plainText, generatedHash);
+		
+		System.out.println("Plain text - "+plainText);
+		System.out.println("Hashed text - "+generatedHash);
+		System.out.println("Check hash and original - "+isMatched);
 	}
 
 }
